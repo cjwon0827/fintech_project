46ec85b81696508b2bfe378157a6e873987dbc48
cjwon0827 <cjwon0827@naver.com>
2024-02-19 19:43:34 +0900

    setup

--- src/main/java/zerobase/fintech/FintechApplication.java
+++ src/main/java/zerobase/fintech/FintechApplication.java
@@ -2,7 +2,9 @@ package zerobase.fintech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FintechApplication {
 
