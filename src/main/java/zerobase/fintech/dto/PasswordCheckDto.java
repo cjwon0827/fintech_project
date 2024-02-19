a141d41f08d908ed6ae429d77a4a7f0f57ee1da5
cjwon0827 <cjwon0827@naver.com>
2024-02-19 19:47:23 +0900

    비밀번호 공통 확인 Dto

--- src/main/java/zerobase/fintech/dto/PasswordCheckDto.java
+++ src/main/java/zerobase/fintech/dto/PasswordCheckDto.java
@@ -1,8 +1,10 @@
 package zerobase.fintech.dto;
 
-import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
 
-@Data
+@Getter
 public class PasswordCheckDto {
+  @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
   private String password;
 }
---
+++ src/main/java/zerobase/fintech/dto/member/LoginDto.java
@@ -0,0 +1,15 @@
+package zerobase.fintech.dto.member;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class LoginDto {
+  @Email
+  @NotBlank(message = "이메일은 공백일 수 없습니다.")
+  private String email;
+
+  @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+  private String password;
+}
